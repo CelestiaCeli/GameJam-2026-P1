6382eac0c153a398c190df3fc283cae58cf69c00
Matheus0027 <matheus.dasilvaoliveira@student.sl.on.ca>
2026-02-25 18:16:17 -0500

    Fixed DeathZone and DeathScreen
    Fixed DeathZone and DeathScreen

--- Assets/Scripts/PlayerDeathScript.cs
+++ Assets/Scripts/PlayerDeathScript.cs
@@ -1,16 +1,29 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerDeathScript : MonoBehaviour
 {
+    [SerializeField]
+    GameObject deathScreen;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.CompareTag("Player"))
+        {
+            deathScreen.SetActive(true);
+            Destroy(collision.gameObject);
+        }
     }
 }
