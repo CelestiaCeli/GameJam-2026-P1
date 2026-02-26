c519f7a3fffd4090ba916accce4df6a977526478
Celi <131928268+CelestiaCeli@users.noreply.github.com>
2026-02-26 10:22:20 -0500

    AUAUAUAUGGHGHTRYTDFYGJFGCHDR

--- Assets/Scripts/DefaultBricks.cs
+++ Assets/Scripts/DefaultBricks.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class DefaultBricks : MonoBehaviour
 {
-    const int HP_DAMAGED = 3;
+    const int HP_DAMAGED = -3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Player player = collision.GetComponent<Player>();
-            player.TakeDamage(HP_DAMAGED);
+            player.ModifyHP(HP_DAMAGED);
         }
     }
 }
--- Assets/Scripts/FireBricks.cs
+++ Assets/Scripts/FireBricks.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class FireBricks : DefaultBricks
 {
-    const int HP_DAMAGED = 8;
+    const int HP_DAMAGED = -8;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Player player = collision.GetComponent<Player>();
-            player.TakeDamage(HP_DAMAGED);
+            player.ModifyHP(HP_DAMAGED);
         }
     }
 }
