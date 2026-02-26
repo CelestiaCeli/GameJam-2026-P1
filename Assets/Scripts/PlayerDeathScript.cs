da48d6b2239d60a95450907cc9d30d9fdaabf8da
Celi <131928268+CelestiaCeli@users.noreply.github.com>
2026-02-25 21:01:40 -0500

    realllly bad :D

--- Assets/Scripts/PlayerDeathScript.cs
+++ Assets/Scripts/PlayerDeathScript.cs
@@ -7,17 +7,6 @@ public class PlayerDeathScript : MonoBehaviour
     [SerializeField]
     GameObject deathScreen;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
